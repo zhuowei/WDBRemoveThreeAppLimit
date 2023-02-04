0e6a792989aa9fe71951d1c08a1b7b519ddb011e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-02-04 10:13:03 -0500

    display success/error messages

--- WDBRemoveThreeAppLimit/ContentView.swift
+++ WDBRemoveThreeAppLimit/ContentView.swift
@@ -8,20 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var message = ""
   var body: some View {
     VStack {
       Image(systemName: "globe")
         .imageScale(.large)
         .foregroundColor(.accentColor)
-      Text("Hello, world!")
+      Text(message)
       Button(action: {
         grant_full_disk_access { error in
           if let error = error {
             print(error)
+            DispatchQueue.main.async {
+              message = "Failed to get full disk access: \(error)"
+            }
             return
           }
           if !patch_installd() {
             print("can't patch installd")
+            DispatchQueue.main.async {
+              message = "Failed to patch installd."
+            }
+            return
+          }
+          DispatchQueue.main.async {
+            message = "Success."
           }
         }
       }) {
